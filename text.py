20caee5816ca178f5bbf04397587c9e56f9d44f3
kimyounsub <tldn0330@naver.com>
2022-03-07 16:15:27 +0900

    추천 시스템 평가하기

---
+++ text.py
@@ -0,0 +1,8 @@
+captains = {}
+captains["Enterprise"] = "Kirk"
+captains["Enterprise D"] = "Picard"
+captains["Deep Space Nine"] = "Sisko"
+captains["Voyager"] = "Janeway"
+
+for ship in captains:
+    print(ship + ":" + captains[ship])
